29f08f7e407fb8558f9bf2cc8de113bad349656b
Canoaafurada <167817182+Canoaafurada@users.noreply.github.com>
2024-06-24 18:12:55 -0300

    Subindo código via upload.

---
+++ a/jojo.js
@@ -0,0 +1,67 @@
+function diavolo() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 10 * i;
+    alert(resultado);
+  }
+}
+function doppio() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 9 * i;
+    alert(resultado);
+  }
+}
+function mista() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 8 * i;
+    alert(resultado);
+  }
+}
+function bruno() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 7 * i;
+    alert(resultado);
+  }
+}
+function giorno() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 6 * i;
+    alert(resultado);
+  }
+}
+function narancia() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 5 * i;
+    alert(resultado);
+  }
+}
+function abachio() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 4 * i;
+    alert(resultado);
+  }
+}
+function trish() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 3 * i;
+    alert(resultado);
+  }
+}
+function koichi() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 2 * i;
+    alert(resultado);
+  }
+}
+function guiaccio() {
+  for (var i = 1; i <= 10; i++) {
+    var resultado = 1 * i;
+    alert(resultado);
+  }
+}
+function jj() {
+  alert("julio");
+}
+
+function j() {
+  alert("joão");
+}
